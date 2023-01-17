52b5ee6d55bf3f5ccbfc967d8201eae75f64c043
Turk <ko.turk@rabobank.nl>
2023-01-18 00:45:58 +0100

    Added live coding project processor

---
+++ VoxxedTicino/PaymentEngine/processor/src/main/kotlin/nl/blue4it/streaming/ProcessorApplication.kt
@@ -0,0 +1,12 @@
+package nl.blue4it.streaming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.EnableKafkaStreams
+
+@SpringBootApplication(scanBasePackages = ["nl.blue4it"])
+open class ProcessorApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProcessorApplication>(*args)
+}
